ac38a348873a09acdfaecb5fedb753e36fec8493
ARCH071995 <99989075+ARCH071995@users.noreply.github.com>
2023-08-01 14:00:28 +0200

    Added the locators or selectors used for automation

---
+++ Resources/PageObject/Locators/locators.py
@@ -0,0 +1,15 @@
+#Home Page Locators
+ClickOnAllAcceptButton = "xpath://button[@title='Alle akzeptieren']"
+ClickOnAnmelden = "xpath://a[@title='Melden Sie sich mit Ihrem Tagesspiegel-Konto an']"
+MessagePopUp = "xpath://iframe[@id='sp_message_iframe_795354']"
+LoginBoxPopUp = "xpath://iframe[@id='modal-iframe']"
+
+# Login Page Locators
+LoginUsernameInputBox = "xpath://input[@id='forms/formLogin_email']"
+LoginPasswordInputBox = "xpath://input[@id='forms/formLogin_password']"
+LoginButton = "xpath://button[@value='Anmelden']"
+
+# Profile Page Locators
+MyAccount = "xpath://span[@class='profile-text']"
+ClickOnMyAccountProfile = "xpath://div[@class='menu__nav__list']//a[@class='sso-link sso-link--my-account'][normalize-space()='Mein Profil']"
+MyUserName = "xpath://span[@class='value'][contains(text(),'qa.tagesspiegel+task@gmail.com')]"
